e87aac8c6ca47a3346e0b5212498b35e9fd77bff
Primi373-creator <120999508+Primi373-creator@users.noreply.github.com>
2024-06-11 18:30:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-
 require("dotenv").config();
+const bodyParser = require('body-parser');
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require('cors')
 
 const app = express();
+
+app.use(cors())
+
+app.use(bodyParser.json());
 app.use(express.json());
 
 const PasteSchema = new mongoose.Schema({
